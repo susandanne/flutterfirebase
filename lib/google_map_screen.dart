4f41cf8ac41d6d413c62576e375b124f64ee64ac
HP <susandanne@gmail.com>
2024-04-23 02:01:32 +0600

    markers google map

--- lib/google_map_screen.dart
+++ lib/google_map_screen.dart
@@ -12,8 +12,32 @@ class _googleMapState extends State<googleMap> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(title: Text('googlemap'),),
-      body:GoogleMap(zoomControlsEnabled:true,initialCameraPosition: CameraPosition(target: LatLng(55.48058772554527, -2.5529188596926833)),) ,
+      appBar: AppBar(
+        title: Text('googlemap'),
+      ),
+      body: GoogleMap(
+        zoomControlsEnabled: true,
+        initialCameraPosition: CameraPosition(
+            target: LatLng(55.48058772554527, -2.5529188596926833),
+             zoom:17,bearing: 30,
+        tilt:20 ),
+        myLocationEnabled: true,
+        myLocationButtonEnabled:true,
+        liteModeEnabled:true ,
+        onTap: (LatLng latLng){
+          print('$latLng is on the map');
+        },
+        markers:{
+          Marker(
+            markerId:MarkerId('Restaurent'),
+            position:LatLng(55.478367048901376, -2.555401213467121),
+            infoWindow: InfoWindow(title:'simply scotish'),
+            icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
+          )
+        } ,
+
+
+      ),
     );
   }
 }
